882c5bc63d418db5dde06cc42b685332d5832717
Oleg <sunfixcrazy@gmail.com>
2023-12-21 13:06:06 +0200

    Fixed conflicts

--- SpringHomework/src/main/java/com/hillel/javaee/controller/ShopController.java
+++ SpringHomework/src/main/java/com/hillel/javaee/controller/ShopController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,7 +43,7 @@ public class ShopController {
     }
 
     @PostMapping(value = "/returnFromBasket")
-    public String returnFromBasket() {
+    public String returnFromBasket(){
         BASKET_PRODUCTS.clear();
         return "redirect:/products";
     }
